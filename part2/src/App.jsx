db98319e07ecf22e08abb9e1044696ff1ee6dcb6
SantLeht <santleht@uef.fi>
2025-09-12 04:12:50 +0300

    newww

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,31 +1,31 @@
-import { PieChart, Pie, Cell, Tooltip } from 'recharts';
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
-const data = [
-  { name: 'A', value: 100 },
-  { name: 'B', value: 100 },
-  { name: 'C', value: 300 },
-  { name: 'D', value: 200 },
-];
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-
-const App = () => (
-  <PieChart width={400} height={400}>
-    <Pie
-      data={data}
-      cx="50%"
-      cy="50%"
-      labelLine={false}
-      label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
-      outerRadius={120}
-      dataKey="value"
-    >
-      {data.map((entry, index) => (
-        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-      ))}
-    </Pie>
-    <Tooltip />
-  </PieChart>
-);
-
-export default App;
+export default App
